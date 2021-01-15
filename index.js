b98eeaecf10b084a08b2bd6d382b71c20e72bff4
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-01-15 00:28:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+const saturdayFun = (para = "roller-skate") => `This Saturday, I want to ${para}!`
+
+const mondayWork = (para = "go to the office") => `This Monday, I will ${para}.`
+
+const wrapAdjective = (para='*') => {
+  return (adj) => {
+    return `You are ${para}${adj}${para}!`   
+  }
+}
+
+const Calculator = {
+  add: (a,b) => {
+    return a + b
+  },
+  subtract: (a,b) => {
+    return a - b
+  },
+  multiply: (a,b) => {
+    return a * b
+  },
+  divide: (a,b) => {
+    return a / b
+  },
+  
+}
+
+
+const actionApplyer = (int, arr) => {
+  arr.forEach((func) => int = func(int) )
+  return int
+};
